f7e67bbacf15e2b43e0c0af8aed716ea80bf677e
Dominick Rivard <drivard@datavalet.com>
2012-10-30 11:14:32 -0400

    Dev:: added support to specify the RAM, and add a feature to set the dns server.

--- README.md
+++ README.md
@@ -2,9 +2,10 @@ OpenVZ Script to automate the creation of container through the command line
 ----------------------------------------------------------------------------
 
 <strike>This script has only one purpose, it is to help me create my OpenVZ containers.</strike>
-Since I use OpenVZ to create development servers I use a predefined amount of RAM
+<del>Since I use OpenVZ to create development servers I use a predefined amount of RAM
 for my servers. The script could be modified in such a way that we could specified
-this amount of RAM through the command line. I will probably end up doing it later.
+this amount of RAM through the command line. I will probably end up doing it later.</del>
+I did add some support for the RAM, check the usage #6.
 
 This script now help me to define some alias shortcuts to manage my OpenVZ server.
 
@@ -20,9 +21,21 @@ Copy the script in your .bashrc file and then execute the command
 #Usage
 1. Stop multiple containers<br/>
 	vzstop 101 102 103
+    
 2. Start multiple containers<br/>
 	vzstart 101 102 103
+    
 3. Destroy multiple containers<br/>
 	vzdestroy 101 102
+    
 4. List all the containers even stopped one<br/>
 	vzlist
+    
+5. Modify the DNS server of the VPS
+    vzadddns 101 8.8.8.8
+    
+6. Create a container with specific amount of RAM
+    vzcreate 101 'centos-6' '192.168.1.101' 1024 2048 centos-6-x86
+    
+7. Create a container with default RAM settings
+    vzcreate 101 'centos-6' '192.168.1.101' centos-6-x86
--- openvztools.sh
+++ openvztools.sh
@@ -19,77 +19,146 @@
 #
 # OpenVZ Bash Alias Shortcuts
 #
+
+######################
+# Path
+#
+PATH_DIR="/usr/sbin"
+
+##########################################
+# Funtions to automate the creation and 
+# the management of the OpenVZ containers
+# 
 function create_vz {
-  VZUID="$1"
-  VZHOSTNAME="$2"
-  VZIP="$3"
-  VZTEMPLATE="$4"
-  if [[ $1 != "" && $2 != "" && $3 != "" && $4 != "" ]]; then
-    # Create an OpenVz container
-    /usr/sbin/vzctl create $VZUID --ostemplate $VZTEMPLATE --config basic
-    # Set the OpenVz container to boot with the machine
-    /usr/sbin/vzctl set $VZUID --onboot yes --save
-    # Set the hostname and the ip address
-    /usr/sbin/vzctl set $VZUID --hostname $VZHOSTNAME --save
-    /usr/sbin/vzctl set $VZUID --ipadd $VZIP --save
-    # Set the number of Sockets and the NameServers
-    /usr/sbin/vzctl set $VZUID --numothersock 360 --save
-    /usr/sbin/vzctl set $VZUID --nameserver 4.2.2.2 --save
-    /usr/sbin/vzctl set $VZUID --nameserver 8.8.8.8 --save
-    /usr/sbin/vzctl set $VZUID --nameserver 8.8.4.4 --save
-    # Set the privvmpages is your memory limit.
-    /usr/sbin/vzctl set $VZUID --vmguarpages $((256 * 512)) --save  # 512MB   Dedicated (Guaranteed)
-    /usr/sbin/vzctl set $VZUID --privvmpages $((256 * 1024)) --save # 1024MB  Burst     (Granted)
-    # Set NFS option on
-    /usr/sbin/vzctl set $VZUID --features "nfs:on" --save
-    # Set Harddisk space
-    /usr/sbin/vzctl set $VZUID --diskspace 3G:4G --save
-    # Sync date/time from host to container
-    /usr/sbin/vzctl set $VZUID --capability sys_time:on --save
-    # Start the container
-    /usr/sbin/vzctl start $VZUID
-    echo ""
-    /usr/sbin/vzlist -a
-  else
-    /bin/echo ""
-    /bin/echo "Usage: "
-    /bin/echo "vzcreate <UID> <HOSTNAME> <IP> <TEMPLATE>"
-    /bin/echo ""
-    /bin/echo ""
-    /usr/sbin/vzlist -a
-  fi
+    NUMBERARGS=$#
+    VZUID="$1"
+    VZHOSTNAME="$2"
+    VZIP="$3"
+    
+    if [[ $NUMBERARGS == 4 ]]; then
+        VZTEMPLATE="$4"
+    fi
+    
+    if [[ $NUMBERARGS == 5 ]]; then
+        VZMEMGARANTEED="$4"
+        VZTEMPLATE="$5"
+    fi
+    
+    if [[ $NUMBERARGS == 6 ]]; then
+        VZMEMGARANTEED="$4"
+        VZMEMGRANTED="$5"
+        VZTEMPLATE="$6"
+    fi    
+    
+    ###################
+    # Main loop
+    # 
+    if [[ $VZUID != "" && $VZHOSTNAME != "" && $VZIP != "" && $VZTEMPLATE != "" ]]; then
+        # Create an OpenVz container
+        $PATH_DIR/vzctl create $VZUID --ostemplate $VZTEMPLATE --config basic
+        # Set the OpenVz container to boot with the machine
+        $PATH_DIR/vzctl set $VZUID --onboot yes --save
+        # Set the hostname and the ip address
+        $PATH_DIR/vzctl set $VZUID --hostname $VZHOSTNAME --save
+        $PATH_DIR/vzctl set $VZUID --ipadd $VZIP --save
+        # Set the number of Sockets and the NameServers
+        $PATH_DIR/vzctl set $VZUID --numothersock 360 --save
+        $PATH_DIR/vzctl set $VZUID --nameserver 8.8.8.8 --save
+        
+        # Set the privvmpages is your memory limit..
+        if [[ $VZMEMGARANTEED != "" ]]; then
+            $PATH_DIR/vzctl set $VZUID --vmguarpages $((256 * $VZMEMGARANTEED)) --save  # xxxMB   Dedicated (Guaranteed)
+        else
+            $PATH_DIR/vzctl set $VZUID --vmguarpages $((256 * 512)) --save  # 512MB   Dedicated (Guaranteed)
+        fi
+        
+        if [[ $VZMEMGRANTED != "" ]]; then
+            $PATH_DIR/vzctl set $VZUID --privvmpages $((256 * $VZMEMGRANTED)) --save # xxxxMB  Burst     (Granted)
+        else
+            $PATH_DIR/vzctl set $VZUID --privvmpages $((256 * 1024)) --save # 1024MB  Burst     (Granted)
+        fi
+        
+        # Set NFS option on
+        $PATH_DIR/vzctl set $VZUID --features "nfs:on" --save
+        # Set Harddisk space
+        $PATH_DIR/vzctl set $VZUID --diskspace 3G:4G --save
+        # Sync date/time from host to container
+        $PATH_DIR/vzctl set $VZUID --capability sys_time:on --save
+        # Start the container
+        $PATH_DIR/vzctl start $VZUID
+        echo ""
+        $PATH_DIR/vzlist -a
+    else
+        /bin/echo ""
+        /bin/echo "Usage: "
+        /bin/echo "vzcreate <UID> <HOSTNAME> <IP> <TEMPLATE>"
+        /bin/echo ""
+        /bin/echo ""
+        $PATH_DIR/vzlist -a
+    fi
 }
+
+
 function destroy_container {
-  for id in $@; do
-    vzctl stop $id;
-    vzctl destroy $id;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl stop $id;
+        $PATH_DIR/vzctl destroy $id;
+    done
 }
+
+
 function onboot_vz_container {
-  for id in $@; do
-    vzctl set $id --onboot yes --save;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl set $id --onboot yes --save;
+    done
 }
+
+
 function offboot_vz_container {
-  for id in $@; do
-    vzctl set $id --onboot no --save;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl set $id --onboot no --save;
+    done
 }
+
+
 function stop_vz {
-  for id in $@; do
-    vzctl stop $id;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl stop $id;
+    done
 }
+
+
 function start_vz {
-  for id in $@; do
-    vzctl start $id;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl start $id;
+    done
 }
+
+
 function restart_vz {
-  for id in $@; do
-    vzctl restart $id;
-  done
+    for id in $@; do
+        $PATH_DIR/vzctl restart $id;
+    done
 }
+
+
+function add_dns {
+    VZUID=($@:0)
+    NUMARGS=($@)
+    NAMESERVERS=${NUMARGS[@]:1}
+    if [[ $# -ge 2 ]]; then
+        for ips in $NAMESERVERS; do
+            $PATH_DIR/vzctl set $VZUID --nameserver $ips --save
+        done
+    else
+        echo "Usage:    vzadddns <UID> <nameserver>"
+    fi
+}
+
+
+########################################## 
+# Alias to call the function defined above
+#
 alias vzlist="vzlist -a"
 alias vzenter="vzctl enter "
 alias vzstop="stop_vz "
@@ -99,3 +168,4 @@ alias vzdestroy="destroy_container "
 alias vzcreate="create_vz "
 alias vzonboot="onboot_vz_container "
 alias vzoffboot="offboot_vz_container "
+alias vzadddns="add_dns "
